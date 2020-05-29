75837913198360b5c0209a1f1b29c092f4e01580
Viet Anh Hoang Van <hoangvanvietanh@gmail.com>
2020-05-29 09:50:04 +0700

    connect with server heroku

--- app/src/main/java/com/android/thongbaogdu/config/Connection.java
+++ app/src/main/java/com/android/thongbaogdu/config/Connection.java
@@ -1,7 +1,7 @@
 package com.android.thongbaogdu.config;
 
 public class Connection {
-    public static final  String URL = "http://192.168.1.31:1200/";
+    public static final  String URL = "https://thongbaogdu-server.herokuapp.com/";
     public static final String URL_SendData = URL+"Ma_so_Xu_ly=Them_Lich_bieu";
     public static final String URL_GetAllAccount = URL+"Ma_so_Xu_ly=Doc_Danh_sach_Nhan_vien";
     public static final String URL_Login = URL+"Ma_so_Xu_ly=login";
